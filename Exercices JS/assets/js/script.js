8897860410088417c79889c22e6a78e67ba95bd8
SidonieLenne <lenne.sidonie@gmail.com>
2020-04-30 11:18:25 +0200

    exercice 5 fini

--- Exercices JS/assets/js/script.js
+++ Exercices JS/assets/js/script.js
@@ -1,3 +1,13 @@
-function carré(nombre){
-    return 3%3
+/*Alternatives:
+function reste(){
+    return (12%5);
+}
+
+function reste(){
+    var douze = 12;
+    var cinq = 5;
+    return (douze%cinq);
+}*/ 
+function reste(){
+ alert(12%5);
 }
