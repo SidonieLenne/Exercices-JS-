9125293dfb5f8288cdfc7d3df110464afaaaccfd
SidonieLenne <lenne.sidonie@gmail.com>
2020-04-30 10:17:26 +0200

    exercice 4 fini et mieux

--- Exercices JS/assets/js/script.js
+++ Exercices JS/assets/js/script.js
@@ -1,3 +1,8 @@
-/*function controle() {
-alert()
-}*/
+function clickfunction(){
+    var firstNameUser = document.getElementById('firstName');
+    var lastNameUser = document.getElementById('lastName');
+    var cityUser = document.getElementById('city');
+ alert('Prénom : ' + firstNameUser.value + '\nNom : '+ lastNameUser.value + '\nVille : '+ cityUser.value)   
+}
+//La fonction, une fois déclarée ne fera rien. Il faut l'appeler pour qu'elle s'exécute, c'est ce qu'on fait avec le onclick du HTML.
+
