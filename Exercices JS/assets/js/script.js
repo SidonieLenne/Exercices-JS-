c6d2bcaf86bc3451df012ed9e84aae97e48ad2e7
SidonieLenne <lenne.sidonie@gmail.com>
2020-04-30 11:58:23 +0200

    exercice 4 corrigé

--- Exercices JS/assets/js/script.js
+++ Exercices JS/assets/js/script.js
@@ -1,8 +1,8 @@
-function clickfunction(){
-    var firstNameUser = document.getElementById('firstName');
-    var lastNameUser = document.getElementById('lastName');
-    var cityUser = document.getElementById('city');
- alert('Prénom : ' + firstNameUser.value + '\nNom : '+ lastNameUser.value + '\nVille : '+ cityUser.value)   
+function clickFunction(){
+    var firstNameUser = document.getElementById('firstName').value;
+    var lastNameUser = document.getElementById('lastName').value;
+    var cityUser = document.getElementById('city').value;
+ alert('Prénom : ' + firstNameUser + '\nNom : '+ lastNameUser + '\nVille : '+ cityUser)   
 }
 //La fonction, une fois déclarée ne fera rien. Il faut l'appeler pour qu'elle s'exécute, c'est ce qu'on fait avec le onclick du HTML.
 
