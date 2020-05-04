4ca7cee39bca0a3a64653862cc57b0c5363d477f
SidonieLenne <lenne.sidonie@gmail.com>
2020-05-04 09:22:06 +0200

    exercice 6 corrigé

--- Exercices JS/assets/js/script.js
+++ Exercices JS/assets/js/script.js
@@ -4,3 +4,11 @@ function reste(){
     var result = (shoe*2+5)*50-birth+1770;
      alert(result);
     }
+
+/*Alternative:
+var result = shoe*2;
+result +=5;   Une façon d'écrire "result = result+5;" plus rapidement.
+result *=50;
+result -=birth;
+result +=1770;
+    */ 
