2cf8043e05e84606b819318fa214ecb4ebb32ad2
SidonieLenne <lenne.sidonie@gmail.com>
2020-05-04 09:59:08 +0200

    exercice 7 corrigé

--- Exercices JS/assets/js/script.js
+++ Exercices JS/assets/js/script.js
@@ -4,11 +4,16 @@ function check() {
     var mineur = 'Vous êtes mineur...';
     //Déclaration de la var qui récupère l'âge entré par l'utilisateur
     var age = document.getElementById('old').value;
-    /*SI l'âge est supérieur à 18, l'alerte contient le message MAJEUR, 
-    SINON, l'alerte contient le message MINEUR*/
-    if (age > 18) {
+    /*SI l'âge est supérieur ou égal à 18 ET inférieur à 120, l'alerte contient le message MAJEUR, 
+    SINON SI l'âge est inférieur à 18 ET supérieur à 0, l'alerte contient le message MINEUR
+    SINON, l'alerte contient le message ERREUR*/
+    if (age >= 18 && age < 120) {
         alert(majeur);
-    } else {
+    }
+    else if (age < 18 && age > 0){
         alert(mineur);
+    }
+    else{
+        alert('erreur');
     }
 }
